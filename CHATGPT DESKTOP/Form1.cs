8cf6c3d54820a6f14bc9b2763593c1ee0b2adc66
Emre Tanrikulu <93709537+TanrikuluEmre@users.noreply.github.com>
2023-08-03 09:27:46 +0300

    Update Form1.cs

--- CHATGPT DESKTOP/Form1.cs
+++ CHATGPT DESKTOP/Form1.cs
@@ -1,11 +1,12 @@
-﻿using System;
-
+using System;
+using System.Drawing;
 using System.IO;
 
 using System.Net;
 using System.Text;
 
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace CHATGPT_DESKTOP
 {
@@ -15,13 +16,15 @@ namespace CHATGPT_DESKTOP
         {
             InitializeComponent();
         }
-        private const string apiKey = "API KEY";
+        private const string apiKey = "apikey";
         private const string modelEndpoint = "https://api.openai.com/v1/chat/completions";
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            this.AcceptButton = button1;
+            darkmodeKapat();
         }
+       
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -30,10 +33,30 @@ namespace CHATGPT_DESKTOP
             // API isteği yap
             string cevap = ChatGPTIstegiYap(metin);
 
+            // karanlık modu cevaba göre kapatma ya da açma işlemi
+            if (cevap.ToLower().Contains("dark mode") || cevap.ToLower().Contains("karanlık mod"))
+            {
+                string soru = "chatgpt seninle bir oyun oynayacağız ve bana sadece 1 karakterlik cevap vereceksin. Kullanıcı sorduğu soruda karanlık modu açmak istiyorsa 1, kapatmak istiyorsa 2, başka bir soru soruyorsa sadece 0 olarak cevap ver.";
+                string darkModeCevap = ChatGPTIstegiYap(soru+" Kullanıcı = (" + metin + ")");
+                switch (darkModeCevap) {
+                    case "1":
+                        darkmodeAc();
+                        break;
+                    case "2":
+                        darkmodeKapat();
+                        break;
+                    
+                }
+
+            }
             // Cevabı ekrana yazdır
+            
+            txtMetin.Clear();
             txtCevap.Text += "Siz: " + metin + Environment.NewLine;
             txtCevap.Text += "ChatGPT: " + cevap + Environment.NewLine;
             txtCevap.Text += "-------------------------------" + Environment.NewLine;
+
+            
         }
 
 
@@ -49,7 +72,7 @@ namespace CHATGPT_DESKTOP
 
                 // GPT-3.5 parametreleri ayarla
                 string gpt3_5Model = "gpt-3.5-turbo";
-                int maxTokens = 50;
+                int maxTokens = 100;
 
                 // İstek verisini oluştur
                 string jsonData = "{\"model\": \"" + gpt3_5Model + "\", \"messages\": [{\"role\": \"system\", \"content\": \"You are a helpful assistant.\"}, {\"role\": \"user\", \"content\": \"" + metin + "\"}], \"max_tokens\": " + maxTokens + "}";
@@ -79,11 +102,38 @@ namespace CHATGPT_DESKTOP
             }
         }
 
+        private void txtMetin_TextChanged(object sender, EventArgs e)
+        {
 
+        }
+        private void darkmodeAc()
+        {
+            txtMetin.BackColor = Color.Black;
+            txtMetin.ForeColor = Color.White;
 
+            txtCevap.BackColor = Color.Black;
+            txtCevap.ForeColor = Color.White;
+        }
+        private void darkmodeKapat()
+        {
+            txtMetin.BackColor = Color.White;
+            txtMetin.ForeColor = Color.Black;
 
+            txtCevap.BackColor = Color.White;
+            txtCevap.ForeColor = Color.Black;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (txtMetin.BackColor==Color.White) {
 
+                darkmodeAc();
+                
+            }
+            else if(txtMetin.BackColor == Color.Black)
+            {
+                darkmodeKapat();
+            }
 
-
+        }
     }
 }
